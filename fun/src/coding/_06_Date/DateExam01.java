f2b60b6066a3555a40e3e890bfc6b5b8755301e1
jaedeokhan <dgd03023@cu.ac.kr>
2021-07-06 02:10:03 +0900

    add Date - 지역화가 고려되어 있지 않고, Java8 Docs에 Depreacted 되어 있는 항목이 많다

---
+++ fun/src/coding/_06_Date/DateExam01.java
@@ -0,0 +1,28 @@
+package coding._06_Date;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateExam01 {
+    public static void main(String[] args) {
+
+        // Date는 JDK 1.0 때 만들어져서 지역화에 대한 부분이 고려되지 않았다.
+        // Calender 클래스는 지역화가 고려되어 있다.
+        
+        Date date = new Date();
+        System.out.println(date.toString());
+
+        // a or p : 오전/오후
+        // zzz : KST
+        SimpleDateFormat ft = new SimpleDateFormat("yyyy.MM.dd 'at' hh:mm:ss a zzz");
+
+        System.out.println(ft.format(date));
+
+        System.out.println(date.getTime()); // 현재시간
+
+        long today = System.currentTimeMillis();
+        System.out.println(today);
+
+        System.out.println(today - date.getTime()); // 걸린시간
+    }
+}
