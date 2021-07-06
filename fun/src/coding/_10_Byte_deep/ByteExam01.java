f6e8568bbf1855ae6cf25651e23e0f49be23f578
jaedeokhan <dgd03023@cu.ac.kr>
2021-07-06 13:05:11 +0900

    add byte IO 심화 1byte씩 읽는 것보다 512byte씩 읽어 오는것이 빠르다. OS에서 파일을 읽을 때 기본적으로 512byte씩 읽어온다. 읽어 올때는 byte[] buffer = new byte[512] 배열을 만들어서 fis.read(buffer)를 추가하고, fos.write(buffer, 0, readCount)와 같이 사용해준다.

--- fun/src/coding/_10_Byte_deep/ByteExam01.java
+++ fun/src/coding/_10_Byte_deep/ByteExam01.java
@@ -1,13 +1,14 @@
-package coding._09_IO;
+package coding._10_Byte_deep;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class ByteExam01 {
-    public static void main(String[] args) throws IOException {
 
+    // 512 byte 씩 읽어 들이기
+
+    public static void main(String[] args) throws IOException {
         long startTime = System.currentTimeMillis();
 
         FileInputStream fis = null;
@@ -17,9 +18,12 @@ public class ByteExam01 {
             fis = new FileInputStream("src/coding/_09_IO/ByteExam01.java");
             fos = new FileOutputStream("src/coding/_09_IO/byte.txt");
 
-            int readData = -1;
-            while ((readData = fis.read()) != -1){
-                fos.write(readData);
+            int readCount = -1;
+            byte[] buffer = new byte[512]; // 512 byte로 배열 생성
+
+            // 1byte씩 가져올때보다 수행시간이 더 빠르다.
+            while ((readCount = fis.read(buffer)) != -1){
+                fos.write(buffer, 0, readCount);
             }
 
         } catch (Exception e){
@@ -31,5 +35,6 @@ public class ByteExam01 {
 
         long endTime = System.currentTimeMillis();
         System.out.println(endTime - startTime);
+
     }
 }
